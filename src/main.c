def50747799dcf5bbb866cbb09623124d548ce73
Zephyr75 <antoine.familleroger@gmail.com>
2022-06-19 00:29:42 +0200

    Started support for bitmap textures

--- src/main.c
+++ src/main.c
@@ -7,10 +7,12 @@
 #include "color.h"
 #include "light.h"
 #include "input.h"
+#include "bitmap_library.h"
 
 Camera cam = {0, 0, -100};
 
 void drawAxis(SDL_Renderer* renderer, Camera cam);
+void drawText(SDL_Renderer* renderer);
 
 int compareTriangles (const void* first, const void* second) 
 {
@@ -30,22 +32,22 @@ int main(int argc, char* argv[])
         if (SDL_CreateWindowAndRenderer(RESOLUTION_X, RESOLUTION_Y, 0, &window, &renderer) == 0) {
             SDL_bool done = SDL_FALSE;
             
-            Vertex a = {-50, -50, 50};
-            Vertex b = {-50, 50, 50};
-            Vertex c = {50, -50, 50};
-            Vertex d = {50, 50, 50};
-            Vertex e = {-50, -50, -50};
-            Vertex f = {-50, 50, -50};
-            Vertex g = {50, -50, -50};
-            Vertex h = {50, 50, -50};
-            Triangle tri1 = {&a, &b, &c, redColor(), redColor()};
-            Triangle tri2 = {&b, &c, &d, redColor(), redColor()};
-            Triangle tri3 = {&e, &f, &g, redColor(), redColor()};
-            Triangle tri4 = {&f, &g, &h, redColor(), redColor()};
-            Triangle tri5 = {&a, &b, &e, redColor(), redColor()};
-            Triangle tri6 = {&b, &e, &f, redColor(), redColor()};
-            Triangle tri7 = {&c, &d, &g, redColor(), redColor()};
-            Triangle tri8 = {&d, &g, &h, redColor(), redColor()};
+            Vertex ver_a = {-50, -50, 50};
+            Vertex ver_b = {-50, 50, 50};
+            Vertex ver_c = {50, -50, 50};
+            Vertex ver_d = {50, 50, 50};
+            Vertex ver_e = {-50, -50, -50};
+            Vertex ver_f = {-50, 50, -50};
+            Vertex ver_g = {50, -50, -50};
+            Vertex ver_h = {50, 50, -50};
+            Triangle tri1 = {&ver_a, &ver_b, &ver_c, redColor(), redColor()};
+            Triangle tri2 = {&ver_b, &ver_c, &ver_d, redColor(), redColor()};
+            Triangle tri3 = {&ver_e, &ver_f, &ver_g, redColor(), redColor()};
+            Triangle tri4 = {&ver_f, &ver_g, &ver_h, redColor(), redColor()};
+            Triangle tri5 = {&ver_a, &ver_b, &ver_e, redColor(), redColor()};
+            Triangle tri6 = {&ver_b, &ver_e, &ver_f, redColor(), redColor()};
+            Triangle tri7 = {&ver_c, &ver_d, &ver_g, redColor(), redColor()};
+            Triangle tri8 = {&ver_d, &ver_g, &ver_h, redColor(), redColor()};
             Mesh* mesh;
             mesh->triangles = calloc(8, sizeof(Triangle));
             mesh->triangles[0] = tri1;
@@ -63,6 +65,12 @@ int main(int argc, char* argv[])
             Light light2 = {100, 0, -50, 30, whiteColor()};
             //SDL_Surface* surface = IMG_Load("resources/brick.jpg");
 
+            /*BMP* bmp = bopen("resources/image.bmp");
+            unsigned int width, height;
+            unsigned char r, g, b;
+            width = get_width(bmp);
+            height = get_height(bmp);*/
+
             while (!done) {
                 SDL_Event event;
 
@@ -79,15 +87,24 @@ int main(int argc, char* argv[])
                 clearMesh(mesh2);
 
 
+                //get_pixel_rgb(bmp, 50, 50, &r, &g, &b);
                 
                 /*
                 qsort(mesh->triangles, mesh->trianglesCount, sizeof(Triangle), compareTriangles);
-                computeLight(light, mesh);
+                //computeLight(light, mesh);
                 //computeLight(light2, mesh);
                 drawMesh(renderer, mesh, cam);
                 */
                
                 qsort(mesh2->triangles, mesh2->trianglesCount, sizeof(Triangle), compareTriangles);
+                /*for (size_t i = 0; i < 50; i++)
+                {
+                    mesh2->vertices[i].baseColor.r = 0;
+                    mesh2->vertices[i].baseColor.g = 0;
+                    mesh2->vertices[i].baseColor.b = 255;
+                }*/
+                
+                
                 computeLight(light, mesh2);
                 computeLight(light2, mesh2);
                 drawMesh(renderer, mesh2, cam);
@@ -113,7 +130,10 @@ int main(int argc, char* argv[])
                         done = SDL_TRUE;
                     }
                 }
+
+                
             }
+            //bclose(bmp);
         }
 
         if (renderer) {
@@ -150,10 +170,30 @@ void drawAxis(SDL_Renderer* renderer, Camera cam){
                 SDL_RenderDrawLine(renderer, z1.x, z1.y, z2.x, z2.y);
 }
 
+// void drawText(SDL_Renderer* renderer){
+// TTF_Font* Sans = TTF_OpenFont("Sans.ttf", 24);
+// SDL_Color White = {255, 255, 255};
+
+// SDL_Surface* surfaceMessage =
+//     TTF_RenderText_Solid(Sans, "put your text here", White); 
+
+// SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+// SDL_Rect Message_rect;
+// Message_rect.x = 0;
+// Message_rect.y = 0;
+// Message_rect.w = 100;
+// Message_rect.h = 100;
+
+// SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+
+// SDL_FreeSurface(surfaceMessage);
+// SDL_DestroyTexture(Message);
+// }
+
 
 /*
-gcc .\src\main.c .\src\vertex.c .\src\triangle.c .\src\constants.c .\src\transform.c .\src\camera.c src/color.c .\src\vector.c .\src\light.c .\src\input.c .\src\mesh.c  -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -g
-
+gcc .\src\main.c .\src\vertex.c .\src\triangle.c .\src\constants.c .\src\transform.c .\src\camera.c src/color.c .\src\vector.c .\src\light.c .\src\input.c .\src\mesh.c .\src\bitmap_library.c  -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -g
 bin/prog.exe
 
 gdb bin/prog.exe
---
+++ test.md
@@ -0,0 +1,7 @@
+# Header 1
+---
+**I'm just trying some stuff** <br>
+*Maybe it's cool*<br>
+~~I don't know~~****
+
+![](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwTFL7G9ZUpHv-9c57kePEmp2pHyRNKC-EuA&usqp=CAU "Link")
